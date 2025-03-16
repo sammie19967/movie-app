4b50105f711a6574bba855e905f15aa304873d4b
Dev. Sammie Barasa <128253322+sammie19967@users.noreply.github.com>
2025-03-16 21:44:07 +0300

    made bottom nav, setup environment looking foward to designing the pages 	modified:   app/(tabs)/_layout.tsx 	modified:   app/(tabs)/index.tsx 	new file:   app/(tabs)/profile.tsx 	new file:   app/(tabs)/saved.tsx 	new file:   app/(tabs)/search.tsx 	modified:   app/_layout.tsx 	new file:   app/global.css 	new file:   app/movies/[id].tsx 	new file:   assets/icons/arrow.png 	new file:   assets/icons/home.png 	new file:   assets/icons/logo.png 	new file:   assets/icons/person.png 	new file:   assets/icons/play.png 	new file:   assets/icons/save.png 	new file:   assets/icons/search.png 	new file:   assets/icons/star.png 	new file:   assets/images/bg.png 	new file:   assets/images/highlight.png 	new file:   assets/images/logo.png 	new file:   assets/images/rankingGradient.png 	new file:   babel.config.js 	new file:   constants/icons.ts 	new file:   constants/images.ts 	new file:   interfaces/interfaces.d.ts 	new file:   metro.config.js 	new file:   nativewind-env.d.ts 	modified:   package-lock.json 	modified:   package.json 	new file:   services/api.ts 	new file:   services/appwrite.ts 	new file:   services/usefetch.ts 	new file:   tailwind.config.js 	modified:   tsconfig.json 	new file:   types/images.d.ts

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,102 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Tabs } from "expo-router";
+import { Image, View } from "react-native";
+import { ImageBackground, Text } from "react-native";
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+
+const TabIcon = ({ focused, icon, title}: any) => {
+  if(focused){
+  return (
+    <ImageBackground
+      source={images.highlight}
+      className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4
+                 justify-center items-center
+                rounded-full overflow-hidden">
+      <Image source={icon}
+        tintColor={"#151312"} className="size-5" ></Image>
+      <Text className="text-secondary text-base font-semibold " >{title}</Text>
+
+
+    </ImageBackground>
+  )
+}
+return(
+  <View className="size-full justify-center items-center mt-4 rounded-full">
+    <Image source={icon} tintColor="#A8B5DB"
+    className="size-5"/>
+  </View>
+)
+}
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
+    screenOptions={{
+      tabBarShowLabel:false,
+      tabBarItemStyle:{
+        width:"100%",
+        height:"100%",
+        justifyContent: "center",
+        alignItems:"center"
+      },
+      tabBarStyle:{
+        backgroundColor: "#0f0d23",
+        borderRadius: 50,
+        marginHorizontal: 20,
+        marginBottom: 36,
+        height: 52,
+        position: "absolute",
+        overflow: 'hidden',
+        borderWidth:1,
+        borderColor:"0f0d23"
+      }
+    }}>
       <Tabs.Screen
         name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+         options={{
+          title: "Home", headerShown: false,
+           tabBarIcon: ({ focused }) => (
+            <TabIcon 
+            focused={focused}
+            icon={icons.home}
+            title="Home"
+            />
+          )
+        }} />
+      <Tabs.Screen name="search"
+       options={{ title: "Search",headerShown:false,
+        tabBarIcon: ({ focused }) => (
+          <TabIcon 
+          focused={focused}
+          icon={icons.search}
+          title="Search"
+          />
+        )
         }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+       />
+       <Tabs.Screen name="saved"
+       options={{ title: "Saved",headerShown:false,
+        tabBarIcon: ({ focused }) => (
+          <TabIcon 
+          focused={focused}
+          icon={icons.save}
+          title="Saved"
+          />
+        )
         }}
-      />
+       />
+      <Tabs.Screen name="profile"
+       options={{ title: "Profile",headerShown:false,
+        tabBarIcon: ({ focused }) => (
+          <TabIcon 
+          focused={focused}
+          icon={icons.person}
+          title="Profile"
+          />
+        )
+        }}
+       />
     </Tabs>
   );
 }
---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class search extends Component {
+  render() {
+    return (
+      <View>
+        <Text>search</Text>
+      </View>
+    )
+  }
+}
+
+export default search
